57914a3e085daa447218b707fffac5b8f2dbd1ff
Orzu Maminov <orzu.maminov9@gmail.com>
2023-01-05 14:21:32 +0600

    Form

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { BiUserPlus } from 'react-icons/bi';
 import Table from '../components/table';
+import Form from '../components/form';
 
 export default function Home() {
 	return (
@@ -24,6 +25,9 @@ export default function Home() {
 							</span>
 						</button>
 					</div>
+				</div>
+				<div className='container mx-auto py-5'>
+					<Form></Form>
 				</div>
 				<div className='container mx-auto'>
 					<Table></Table>
