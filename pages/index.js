89ed63698f44107ac4450c0e57fe2ac587596f6c
Orzu Maminov <orzu.maminov9@gmail.com>
2023-01-06 16:15:45 +0600

    toggle

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,15 @@ import Head from 'next/head';
 import { BiUserPlus } from 'react-icons/bi';
 import Table from '../components/table';
 import Form from '../components/form';
+import { useState } from 'react';
 
 export default function Home() {
+	const [visible, setVisible] = useState(false);
+
+	const handler = () => {
+		setVisible(!visible);
+	};
+
 	return (
 		<section>
 			<Head>
@@ -18,7 +25,10 @@ export default function Home() {
 				</h1>
 				<div className='container mx-auto flex justify-between py-5 border-b'>
 					<div className='left flex gap-3'>
-						<button className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-gray-800 '>
+						<button
+							onClick={handler}
+							className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-gray-800 '
+						>
 							Add Employee{' '}
 							<span className='px-1'>
 								<BiUserPlus size={23}></BiUserPlus>
@@ -26,9 +36,7 @@ export default function Home() {
 						</button>
 					</div>
 				</div>
-				<div className='container mx-auto py-5'>
-					<Form></Form>
-				</div>
+				{visible ? <Form></Form> : <></>}
 				<div className='container mx-auto'>
 					<Table></Table>
 				</div>
